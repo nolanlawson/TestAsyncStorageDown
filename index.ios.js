decd5f2c09eb49b146ba71d0f440d0b531b5fa83
Nolan Lawson <nolan.lawson@gmail.com>
2015-09-20 15:24:22 -0400

    add asyncstorage-down

--- index.ios.js
+++ index.ios.js
@@ -12,6 +12,31 @@ var {
   View,
 } = React;
 
+var asyncstorage = require('asyncstorage-down');
+var levelup = require('levelup');
+var db = levelup('/does/not/matter', { db: asyncstorage });
+
+db.put('name', 'Yuri Irsenovich Kim');
+db.put('dob', '16 February 1941');
+db.put('spouse', 'Kim Young-sook');
+db.put('occupation', 'Clown');
+
+db.readStream()
+   .on('data', function (data) {
+      if (typeof data.value !== 'undefined') {
+         console.log(data.key, '=', data.value);
+      }
+   })
+   .on('error', function (err) {
+      console.log('Oh my!', err);
+   })
+   .on('close', function () {
+      console.log('Stream closed');
+   })
+   .on('end', function () {
+     console.log('Stream ended');
+   });
+
 var TestAsyncStorageDown = React.createClass({
   render: function() {
     return (
